54e81e306f5161738878c07a8e6079f3549af7a3
taldanai1995 <taldanai@icloud.com>
2024-01-19 10:27:01 +0200

    add example

---
+++ Example.java
@@ -0,0 +1,7 @@
+public class Example {
+    public static void main(String[] args) {
+        int number = 1;   
+        int number1 = 2;
+        System.out.println(number * number1);//3
+    }
+}
